5629c28e2435bba8373fdb5284c2d972864fe01e
JoseRomualdo <jromualdo3@hotmail.com>
2026-07-22 10:46:30 -0300

    Feat: novo controlador auth.

--- Backend/src/main/java/br/com/FinanceAi/Backend/controller/AuthController.java
+++ Backend/src/main/java/br/com/FinanceAi/Backend/controller/AuthController.java
@@ -1,4 +1,33 @@
 package br.com.FinanceAi.Backend.controller;
 
+import br.com.FinanceAi.Backend.dto.request.RegisterRequest;
+import br.com.FinanceAi.Backend.dto.response.RegisterResponse;
+import br.com.FinanceAi.Backend.entity.Usuario;
+import br.com.FinanceAi.Backend.mapper.UsuarioMapper;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("usuarios")
 public class AuthController {
+
+    private UsuarioMapper usuarioMapper;
+
+    public AuthController(UsuarioMapper usuarioMapper) {
+        this.usuarioMapper = usuarioMapper;
+    }
+
+    @PostMapping("registrar")
+    public ResponseEntity<RegisterResponse> registrar(@RequestBody @Valid RegisterRequest registerRequest, UriComponentsBuilder uri) {
+
+        Usuario usuario = usuarioMapper.map(registerRequest);
+
+        return
+    }
+
 }
