51b8d70783f895fe493bab8b5f5238015ee67f44
JoseRomualdo <jromualdo3@hotmail.com>
2026-08-03 14:37:43 -0300

    feat(AuthService): add service do auth.

--- Backend/src/main/java/br/com/FinanceAi/Backend/service/AuthService.java
+++ Backend/src/main/java/br/com/FinanceAi/Backend/service/AuthService.java
@@ -1,4 +1,24 @@
 package br.com.FinanceAi.Backend.service;
 
-public class AuthService {
+import br.com.FinanceAi.Backend.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public AuthService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + email));
+    }
+
 }
