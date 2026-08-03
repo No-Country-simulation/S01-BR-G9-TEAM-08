95aa46cb9347a3f46d0e43ef62066126e554a69c
JoseRomualdo <jromualdo3@hotmail.com>
2026-08-03 14:37:03 -0300

    feat(AuthController): add controller.

--- Backend/src/main/java/br/com/FinanceAi/Backend/controller/AuthController.java
+++ Backend/src/main/java/br/com/FinanceAi/Backend/controller/AuthController.java
@@ -1,33 +1,68 @@
 package br.com.FinanceAi.Backend.controller;
 
-import br.com.FinanceAi.Backend.dto.request.RegisterRequest;
-import br.com.FinanceAi.Backend.dto.response.RegisterResponse;
+import br.com.FinanceAi.Backend.dto.request.LoginRequest;
+import br.com.FinanceAi.Backend.dto.request.usuario.UsuarioCadastraRequest;
+import br.com.FinanceAi.Backend.dto.response.LoginResponse;
+import br.com.FinanceAi.Backend.dto.response.usuario.UsuarioCadastraResponse;
 import br.com.FinanceAi.Backend.entity.Usuario;
-import br.com.FinanceAi.Backend.mapper.UsuarioMapper;
+import br.com.FinanceAi.Backend.service.TokenService;
+import br.com.FinanceAi.Backend.service.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.time.Duration;
+
 @RestController
-@RequestMapping("usuarios")
+@RequestMapping("auth")
 public class AuthController {
 
-    private UsuarioMapper usuarioMapper;
+    private final TokenService tokenService;
+    private UsuarioService usuarioService;
+    private AuthenticationManager authenticationManager;
 
-    public AuthController(UsuarioMapper usuarioMapper) {
-        this.usuarioMapper = usuarioMapper;
+    public static final String TOKEN_TYPE = "Bearer";
+    public static final long EXPIRA_TOKEN = Duration.ofHours(2).getSeconds(); // 7200
+
+    public AuthController(
+            UsuarioService usuarioService,
+            TokenService tokenService,
+            AuthenticationManager authenticationManager
+    ) {
+        this.usuarioService = usuarioService;
+        this.tokenService = tokenService;
+        this.authenticationManager = authenticationManager;
     }
 
-    @PostMapping("registrar")
-    public ResponseEntity<RegisterResponse> registrar(@RequestBody @Valid RegisterRequest registerRequest, UriComponentsBuilder uri) {
+    @PostMapping("/register")
+    public ResponseEntity<UsuarioCadastraResponse> cadastrar(@RequestBody @Valid UsuarioCadastraRequest request, UriComponentsBuilder uri) {
 
-        Usuario usuario = usuarioMapper.map(registerRequest);
+        UsuarioCadastraResponse response = usuarioService.cadastrar(request);
 
-        return
+        URI location = uri
+                .path("/usuarios/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
+
+        UsernamePasswordAuthenticationToken emailSenha = new UsernamePasswordAuthenticationToken(request.email(), request.senha());
+
+        var auth = this.authenticationManager.authenticate(emailSenha);
+        var token = tokenService.generateToken((Usuario) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponse(token, TOKEN_TYPE, EXPIRA_TOKEN));
     }
 
 }
