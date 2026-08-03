a975d24bcc67e26035472f62aa9f009f1b971c5b
JoseRomualdo <jromualdo3@hotmail.com>
2026-08-03 15:24:35 -0300

    feat(Usuario): add entity usuario.

--- Backend/src/main/java/br/com/FinanceAi/Backend/entity/Usuario.java
+++ Backend/src/main/java/br/com/FinanceAi/Backend/entity/Usuario.java
@@ -1,4 +1,60 @@
 package br.com.FinanceAi.Backend.entity;
 
-public class Usuario {
+import br.com.FinanceAi.Backend.entity.enums.SituacaoConta;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.OffsetDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private OffsetDateTime dataCadastro;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private SituacaoConta situacaoConta = SituacaoConta.ATIVO;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
