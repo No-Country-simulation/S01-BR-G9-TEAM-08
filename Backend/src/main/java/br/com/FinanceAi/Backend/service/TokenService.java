4a3eddeed40db027880d7b5f0f521d54f4035787
JoseRomualdo <jromualdo3@hotmail.com>
2026-08-03 14:47:25 -0300

    feat(TokenService): add enum token service.

---
+++ Backend/src/main/java/br/com/FinanceAi/Backend/service/TokenService.java
@@ -0,0 +1,53 @@
+package br.com.FinanceAi.Backend.service;
+
+import br.com.FinanceAi.Backend.entity.Usuario;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+
+
+@Service
+public class TokenService {
+
+    @Value("${JWT_SECRET:my-secret-key}")
+    private String secret;
+
+    @Value("${JWT_EXPIRATION_HOURS:2}")
+    private long expirationHours;
+
+    public String generateToken(Usuario usuario) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(usuario.getUsername())
+                    .withExpiresAt(genExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar o token", exception);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return "";
+        }
+    }
+
+    private Instant genExpirationDate() {
+        return Instant.now().plus(Duration.ofHours(expirationHours));
+    }
+}
