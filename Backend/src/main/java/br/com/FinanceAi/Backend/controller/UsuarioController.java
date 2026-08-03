2487c23e88e696306356f1a97b884e3358e31299
JoseRomualdo <jromualdo3@hotmail.com>
2026-08-03 15:27:58 -0300

    feat(UsuarioController): add controller do usuario.

---
+++ Backend/src/main/java/br/com/FinanceAi/Backend/controller/UsuarioController.java
@@ -0,0 +1,58 @@
+package br.com.FinanceAi.Backend.controller;
+
+import br.com.FinanceAi.Backend.dto.request.usuario.UsuarioAlteraSenhaRequest;
+import br.com.FinanceAi.Backend.dto.request.usuario.UsuarioAtualizaRequest;
+import br.com.FinanceAi.Backend.dto.request.usuario.UsuarioExcluiRequest;
+import br.com.FinanceAi.Backend.dto.response.usuario.UsuarioAtualizaResponse;
+import br.com.FinanceAi.Backend.dto.response.usuario.UsuarioResponse;
+import br.com.FinanceAi.Backend.entity.Usuario;
+import br.com.FinanceAi.Backend.mapper.UsuarioMapper;
+import br.com.FinanceAi.Backend.service.UsuarioService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+    private final UsuarioMapper usuarioMapper;
+
+    @GetMapping("/me")
+    public ResponseEntity<UsuarioResponse> obterPerfil(@AuthenticationPrincipal Usuario usuarioAutenticado) {
+        UsuarioResponse response = usuarioService.buscarPerfil(usuarioAutenticado.getId());
+       return ResponseEntity.ok(usuarioMapper.toUsuarioResponse(usuarioAutenticado));
+    }
+
+    @PutMapping("/me")
+    public ResponseEntity<UsuarioAtualizaResponse> atualizarPerfil(
+            @AuthenticationPrincipal Usuario usuarioAutenticado,
+            @RequestBody @Valid UsuarioAtualizaRequest request
+    ) {
+        UsuarioAtualizaResponse response = usuarioService.atualizaPerfil(usuarioAutenticado.getId(), request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/me/senha")
+    public ResponseEntity<Void> alterarSenha(
+            @AuthenticationPrincipal Usuario usuarioAutenticado,
+            @RequestBody @Valid UsuarioAlteraSenhaRequest request
+    ) {
+        usuarioService.alteraSenha(usuarioAutenticado.getId(), request);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/me")
+    public ResponseEntity<Void> removerPerfil(
+            @AuthenticationPrincipal Usuario usuarioAutenticado,
+            @RequestBody @Valid UsuarioExcluiRequest request
+    ) {
+        usuarioService.excluiUsuario(usuarioAutenticado.getId(), request);
+        return ResponseEntity.noContent().build();
+    }
+}
