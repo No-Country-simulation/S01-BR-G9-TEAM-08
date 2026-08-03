f63973d11c340be35dbdec1a896f20c48675b69a
JoseRomualdo <jromualdo3@hotmail.com>
2026-08-03 15:26:19 -0300

    feat(UsuarioCadastraRequest): add request de cadastro de usuario.

---
+++ Backend/src/main/java/br/com/FinanceAi/Backend/dto/request/usuario/UsuarioCadastraRequest.java
@@ -0,0 +1,21 @@
+package br.com.FinanceAi.Backend.dto.request.usuario;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UsuarioCadastraRequest(
+        @NotBlank
+        @Size(min = 6, max = 100, message = "Nome deve ter entre 2 e 100 caracteres")
+        String nome,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Size(min = 6, max = 100, message = "Senha deve ter no mínimo de 6 caracteres")
+        String senha
+
+) {
+}
