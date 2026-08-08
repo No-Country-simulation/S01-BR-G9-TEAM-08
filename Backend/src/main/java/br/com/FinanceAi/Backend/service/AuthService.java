3d727a1f7bd285502b00f54519c75b15cea76db9
JoseRomualdo <jromualdo3@hotmail.com>
2026-08-08 09:47:34 -0300

    refactor(AuthService): refatoração do loadUserByUserName.

--- Backend/src/main/java/br/com/FinanceAi/Backend/service/AuthService.java
+++ Backend/src/main/java/br/com/FinanceAi/Backend/service/AuthService.java
@@ -1,5 +1,7 @@
 package br.com.FinanceAi.Backend.service;
 
+import br.com.FinanceAi.Backend.config.UsuarioAutenticado;
+import br.com.FinanceAi.Backend.entity.Usuario;
 import br.com.FinanceAi.Backend.repository.UsuarioRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,8 +19,19 @@ public class AuthService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return usuarioRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + email));
-    }
 
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Usuário não encontrado: " + email)
+                );
+
+        return UsuarioAutenticado.builder()
+                .id(usuario.getId())
+                .nome(usuario.getNome())
+                .email(usuario.getEmail())
+                .senha(usuario.getSenha())
+                .dataCadastro(usuario.getDataCadastro())
+                .situacaoConta(usuario.getSituacaoConta())
+                .build();
+    }
 }
